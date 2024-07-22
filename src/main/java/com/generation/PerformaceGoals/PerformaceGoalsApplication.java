960e713c2f7b4081b6ff47d837e198b0a6ca7e56
Isabelly Dornelas <isabelly.dornelas123@gmail.com>
2024-07-22 11:16:31 -0300

    configurando o projeto

---
+++ src/main/java/com/generation/PerformaceGoals/PerformaceGoalsApplication.java
@@ -0,0 +1,13 @@
+package com.generation.PerformaceGoals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformaceGoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformaceGoalsApplication.class, args);
+	}
+
+}
